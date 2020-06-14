c64788e1e458a7a0fc288e65ea9c8a2b5d52554c
Bychrist <ordained07@yahoo.com>
2020-06-14 19:06:02 +0100

    Adding Controller for the index page

---
+++ PayCompute/Controllers/EmployeeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PayCompute.Models;
+using PayCompute.Services;
+
+namespace PayCompute.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        public IActionResult    Index()
+        {
+            IEnumerable<EmployeeIndexViewModel> employees = (IEnumerable<EmployeeIndexViewModel>) _employeeService.GetAll();
+            
+            return View(employees);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}

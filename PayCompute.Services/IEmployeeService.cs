4e23a3fc0715fd5b1c906c7c8f210f2d5169aeba
Bychrist <ordained07@yahoo.com>
2020-06-14 18:23:52 +0100

    This is the Employee service implementation

--- PayCompute.Services/IEmployeeService.cs
+++ PayCompute.Services/IEmployeeService.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using PayCompute.Entity;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace PayCompute.Services
 {
-    class IEmployeeService
+    public interface IEmployeeService
     {
+        Task CreateAsync(Employee newEmployee);
+        Employee GetById(int employeeid);
+        Task UpdateAsync(Employee employee);
+        Task UpdateAsync(int id);
+        Task Delete(int employeeId);
+        decimal UnionFees(int id);
+        decimal StudentLoanRepaymentAmount(int id, decimal totalAmount);
+        IEnumerable<Employee> GetAll();
+
+
+
     }
 }
--- PayCompute.Services/Implementation/EmployeeService.cs
+++ PayCompute.Services/Implementation/EmployeeService.cs
@@ -1,10 +1,59 @@
-﻿using System;
+﻿using PayCompute.Entity;
+using PayCompute.Persistence;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace PayCompute.Services.Implementation
 {
-    class EmployeeService
+    public class EmployeeService : IEmployeeService
     {
+        private readonly ApplicationDbContext db;
+        public EmployeeService(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+        public async Task CreateAsync(Employee newEmployee)
+        {
+            await db.AddAsync(newEmployee);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task Delete(int employeeId)
+        {
+            var employee = GetById(employeeId);
+            db.Remove(employee);
+            await db.SaveChangesAsync();
+        }
+
+        public IEnumerable<Employee> GetAll() => db.Employees.ToList();
+
+        public async Task UpdateAsync(Employee employee)
+        {
+            db.Update(employee);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(int id)
+        {
+            var employee = GetById(id);
+            db.Update(employee);
+            await db.SaveChangesAsync();
+        } 
+
+        public Employee GetById(int employeeid) => db.Employees.Where(e => e.Id == employeeid).FirstOrDefault();
+        public decimal StudentLoanRepaymentAmount(int id, decimal totalAmount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal UnionFees(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+       
     }
 }
